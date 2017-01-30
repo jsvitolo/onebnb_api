f9e0393f9d57494ee663bf1febcc9f3c59f24041
Jones Vitolo <jsvitolo@gmail.com>
2017-01-30 20:14:53 -0200

    mailcacher config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  # configuração do mailcacher
+  config.action_mailer.default_url_options = { :host => 'localhost:4200' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 }
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
